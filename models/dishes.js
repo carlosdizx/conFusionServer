47df6c524493497b46c0d03fbcc97fd3a32e2c75
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-03 16:51:17 -0500

    Agregado nuevos campos para Dish

--- models/dishes.js
+++ models/dishes.js
@@ -8,6 +8,11 @@ const commentSchema = new Schema(
     rating: { type: Number, required: true, min: 1, max: 5 },
     comment: { type: String, required: true },
     author: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    label: { type: String, default: "" },
+    price: { type: Currency, required: true, min: 0 },
+    featured: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
