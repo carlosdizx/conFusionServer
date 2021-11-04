0334c9bc334c5a419373fb1cc3295d907bb2df31
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-04 12:26:21 -0500

    Creado el modelo que soportara las operaciones para Leaders

---
+++ models/leaders.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const leadersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    designation: { type: String, required: true },
+    abbr: { type: String, required: true },
+    description: { type: String, required: true },
+    featured: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Leaders = mongoose.model("Leaders", leadersSchema);
+
+module.exports = Leaders;
