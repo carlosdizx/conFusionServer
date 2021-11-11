2f5180d134db5b6a3f00cfc393e596543db1b19a
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-11 12:01:29 -0500

    Creado metodo de singup para los usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const bodyParser = require("body-parser");
+const User = require("../models/user");
+
+const router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
+});
+
+router.post("/singup", function (req, res, next) {
+  User.findOne({ username: req.body.user })
+    .then((user) => {
+      if (user) {
+        const error = new Error(
+          "User " + req.body.username + " already exists!"
+        );
+        error.status = 403;
+        next(error);
+      } else {
+        return User.create({
+          username: req.body.username,
+          password: req.body.password,
+        });
+      }
+    })
+    .then(
+      (user) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ status: "success", user: user });
+      },
+      (err) => {
+        next(err);
+      }
+    )
+    .catch((err) => next(err));
 });
 
 module.exports = router;
