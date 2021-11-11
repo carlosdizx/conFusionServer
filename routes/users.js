ba8d18c568a7e18c7363b32c201e6b3d03118dbc
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-11 12:17:58 -0500

    Creado metodo de login obitnenedo el usuario desde la base de datos

--- routes/users.js
+++ routes/users.js
@@ -39,4 +39,50 @@ router.post("/singup", function (req, res, next) {
     .catch((err) => next(err));
 });
 
+router.post("/login", (req, res, next) => {
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+
+    const auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    const username = auth[0];
+    const password = auth[1];
+
+    User.findOne({ username: username })
+      .then((user) => {
+        if (!user) {
+          const err = new Error("User " + user + " does not exists");
+          res.setHeader("WWW-Authenticate", "Basic");
+          err.status = 403;
+          return next(err);
+        } else if (user.password !== password) {
+          const err = new Error("Your password is incorrect!");
+          res.setHeader("WWW-Authenticate", "Basic");
+          err.status = 403;
+          return next(err);
+        } else if (user.username === username && user.password === password) {
+          req.session.user = "authenticated";
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("You are authenticated!");
+        }
+      })
+      .catch((err) => next(err));
+  }
+  else{
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("You are already authenticated!")
+  }
+});
+
 module.exports = router;
