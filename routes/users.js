ba3d2b8a718a8bcbc365a257c6981fdb7c8c326c
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-11 20:32:01 -0500

    Terminado el sistema de autenticacion para los usuarios

--- routes/users.js
+++ routes/users.js
@@ -25,56 +25,17 @@ router.post("/singup", (req, res, next) => {
         passport.authenticate("local")(req, res, () => {
           res.statusCode = 200;
           res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "success" });
+          res.json({ success: true, status: "Singup" });
         });
       }
     }
   );
 });
 
-router.post("/login", (req, res, next) => {
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-
-    const auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const username = auth[0];
-    const password = auth[1];
-
-    User.findOne({ username: username })
-      .then((user) => {
-        if (!user) {
-          const err = new Error("User " + username + " does not exists");
-          res.setHeader("WWW-Authenticate", "Basic");
-          err.status = 403;
-          return next(err);
-        } else if (user.password !== password) {
-          const err = new Error("Your password is incorrect!");
-          res.setHeader("WWW-Authenticate", "Basic");
-          err.status = 403;
-          return next(err);
-        } else if (user.username === username && user.password === password) {
-          req.session.user = "authenticated";
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.end("You are authenticated!");
-        }
-      })
-      .catch((err) => next(err));
-  } else {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("You are already authenticated!");
-  }
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({ success: true, status: "SingIn" });
 });
 
 router.get("/logout", (req, res, next) => {
