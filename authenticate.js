50b629c75126e063909f60c8755fa56155421a35
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-11 21:21:27 -0500

    creado elarchivo de configuracion para la llave secreta y la url de la base de datos

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,38 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user");
+const JwtStrategy = require("app passport-jwt").Strategy;
+const ExtractJwtStrategy = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken");
+const { ExtractJwt } = require("passport-jwt");
+const config = require("./config");
+const { jwtPassport } = require("./authenticate");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = (user) => {
+  return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
+};
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.secretKey,
+};
+
+exports.jwtPassport = passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log(jwt_payload);
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
+      if (err) {
+        return done(err, false);
+      } else if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
+
+exports.verifyUser = passport.authenticate('jwt',{session:false})
