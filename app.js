cafaabd3e3a3fef792e5f46d1865feaa29348410
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-10 10:45:48 -0500

    :fire:

--- app.js
+++ app.js
@@ -38,13 +38,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-09876-54321"));
 
-
 app.use(passport.initialize());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-
 
 app.use(express.static(path.join(__dirname, "public")));
 
